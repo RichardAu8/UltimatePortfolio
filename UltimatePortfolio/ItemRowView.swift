78ec854fd7010bd5a46e9ad6edcab2bd6b980e40
Richard Au <richardwau@gmail.com>
2022-04-04 15:48:47 +1000

    Complete to 7. Editing Items

---
+++ UltimatePortfolio/ItemRowView.swift
@@ -0,0 +1,24 @@
+//
+//  ItemRowView.swift
+//  UltimatePortfolio
+//
+//  Created by RICHARD AU on 4/4/2022.
+//
+
+import SwiftUI
+
+struct ItemRowView: View {
+    @ObservedObject var item: Item
+    
+    var body: some View {
+        NavigationLink(destination: EditItemView(item: item)) {
+            Text(item.itemTitle)
+        }
+    }
+}
+
+struct ItemRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        ItemRowView(item: Item.example)
+    }
+}
--- UltimatePortfolio/ProjectsView.swift
+++ UltimatePortfolio/ProjectsView.swift
@@ -30,10 +30,9 @@ struct ProjectsView: View {
                 ForEach(projects.wrappedValue) { project in
                     Section(header: Text(project.projectTitle)) {
                         ForEach(project.projectItems) {item in
-                            Text(item.itemTitle)
+                            ItemRowView(item: item)
                         }
                     }
-                    
                 }
             }
             .listStyle(InsetGroupedListStyle())
