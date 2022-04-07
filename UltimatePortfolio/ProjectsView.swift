0d39c5797f0d18df3539bd7be4c1c4a306e67849
Richard Au <richardwau@gmail.com>
2022-04-07 11:45:09 +1000

    12. Adding & Deleting - complete but has run time warnings for layout and core data

--- UltimatePortfolio/ProjectsView.swift
+++ UltimatePortfolio/ProjectsView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct ProjectsView: View {
+    @EnvironmentObject var dataController: DataController
+    @Environment(\.managedObjectContext) var managedObjectContext
+    
     static let openTag: String = "Open"
     static let closedTag: String = "Closed"
     
@@ -32,13 +35,46 @@ struct ProjectsView: View {
                         ForEach(project.projectItems) {item in
                             ItemRowView(item: item)
                         }
+                        .onDelete { offsets in
+                            let allItems = project.projectItems
+                            for offset in offsets {
+                                let item = allItems[offset]
+                                dataController.delete(item)
+                            }
+                            dataController.save()
+                        }
+                        if showClosedProjects == false {
+                            Button {
+                                withAnimation {
+                                    let item = Item(context: managedObjectContext)
+                                    item.project = project
+                                    item.creationDate = Date()
+                                    dataController.save()
+                                }
+                            } label: {
+                                Label("Add New Item", systemImage: "plus")
+                            }
+                        }
                     }
                 }
             }
             .listStyle(InsetGroupedListStyle())
             .navigationTitle(showClosedProjects ? "Closed Projects" : "Open Projects")
+            .toolbar {
+                if showClosedProjects == false {
+                    Button {
+                        withAnimation {
+                            let project = Project(context: managedObjectContext)
+                            project.closed = false
+                            project.creationDate = Date()
+                            dataController.save()
+                        }
+                    } label: {
+                        Label("Add Project", systemImage: "plus")
+                    }
+                }
+            }
         }
-        
     }
 }
 
