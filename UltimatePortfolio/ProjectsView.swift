a0ef1331caf2e078603d9e8941bb1d0ffdd81195
Richard Au <richardwau@gmail.com>
2022-04-14 13:23:25 +1000

    14. Finishing ProjectsView - complete but doesn't render properly given NavigationView was declared multiple times in the view hierarchy instead of ONCE at the top.  Also the Bug Paul fixes for landscape mode no longer exists in iOS 15.x

--- UltimatePortfolio/ProjectsView.swift
+++ UltimatePortfolio/ProjectsView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ProjectsView: View {
     @EnvironmentObject var dataController: DataController
     @Environment(\.managedObjectContext) var managedObjectContext
-        
+    
     static let openTag: String = "Open"
     static let closedTag: String = "Closed"
     
@@ -34,71 +34,77 @@ struct ProjectsView: View {
         // NOTE Hudson makes a mistake here, Navigation view should only appear at the top of the view hierarchy
         // otherwise the view gets smaller everytime you navigate down the hierarchy
         NavigationView {
-            List {
-                ForEach(projects.wrappedValue) { project in
-                    Section(header: ProjectHeaderView(project: project)) {
-                        ForEach(project.projectItems(using: sortOrder)) {item in
-                            ItemRowView(item: item)
-                        }
-                        .onDelete { offsets in
-                            let allItems = project.projectItems
-                            for offset in offsets {
-                                let item = allItems[offset]
-                                dataController.delete(item)
-                            }
-                            dataController.save()
-                        }
-                        if showClosedProjects == false {
-                            Button {
-                                withAnimation {
-                                    let item = Item(context: managedObjectContext)
-                                    item.project = project
-                                    item.creationDate = Date()
+            Group {
+                if projects.wrappedValue.count == 0 {
+                    Text("There's nothing here right now.")
+                        .foregroundColor(.secondary)
+                } else {
+                    List {
+                        ForEach(projects.wrappedValue) { project in
+                            Section(header: ProjectHeaderView(project: project)) {
+                                ForEach(project.projectItems(using: sortOrder)) {item in
+                                    ItemRowView(project: project, item: item)
+                                }
+                                .onDelete { offsets in
+                                    let allItems = project.projectItems(using: sortOrder)
+                                    for offset in offsets {
+                                        let item = allItems[offset]
+                                        dataController.delete(item)
+                                    }
                                     dataController.save()
                                 }
+                                if showClosedProjects == false {
+                                    Button {
+                                        withAnimation {
+                                            let item = Item(context: managedObjectContext)
+                                            item.project = project
+                                            item.creationDate = Date()
+                                            dataController.save()
+                                        }
+                                    } label: {
+                                        Label("Add New Item", systemImage: "plus")
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    .listStyle(InsetGroupedListStyle())
+                    .navigationTitle(showClosedProjects ? "Closed Projects" : "Open Projects")
+                    .toolbar {
+                        ToolbarItem(placement: .navigationBarTrailing) {
+                            if showClosedProjects == false {
+                                Button {
+                                    withAnimation {
+                                        let project = Project(context: managedObjectContext)
+                                        project.closed = false
+                                        project.creationDate = Date()
+                                        dataController.save()
+                                    }
+                                } label: {
+                                    Label("Add Project", systemImage: "plus")
+                                }
+                            }
+                        }
+                        ToolbarItem(placement: .navigationBarLeading) {
+                            Button {
+                                showingSortOrder.toggle()
                             } label: {
-                                Label("Add New Item", systemImage: "plus")
+                                Label("Sort", systemImage: "arrow.up.arrow.down")
                             }
                         }
                     }
-                }
-            }
-            .listStyle(InsetGroupedListStyle())
-            .navigationTitle(showClosedProjects ? "Closed Projects" : "Open Projects")
-            
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    if showClosedProjects == false {
-                        Button {
-                            withAnimation {
-                                let project = Project(context: managedObjectContext)
-                                project.closed = false
-                                project.creationDate = Date()
-                                dataController.save()
-                            }
-                        } label: {
-                            Label("Add Project", systemImage: "plus")
-                        }
+                    .actionSheet(isPresented: $showingSortOrder) {
+                        ActionSheet(title: Text("Sort Items"), message: nil, buttons: [
+                            .default(Text("Optimized")) {sortOrder = .optimized },
+                            .default(Text("Creation Date")) {sortOrder = .creationDate },
+                            .default(Text("Title")) {sortOrder = .title }
+                        ])
                     }
-                }
-                ToolbarItem(placement: .navigationBarLeading) {
-                    Button {
-                        showingSortOrder.toggle()
-                    } label: {
-                        Label("Sort", systemImage: "arrow.up.arrow.down")
-                    }
-                }
-            }
-            .actionSheet(isPresented: $showingSortOrder) {
-                ActionSheet(title: Text("Sort Items"), message: nil, buttons: [
-                    .default(Text("Optimized")) {sortOrder = .optimized },
-                    .default(Text("Creation Date")) {sortOrder = .creationDate },
-                    .default(Text("Title")) {sortOrder = .title }
-                ])
-            }
-        }
+                } // else
+            } // Group
+            SelectSomethingView()
+        } //NavigationView
     }
-
 }
 
 struct ProjectsView_Previews: PreviewProvider {
