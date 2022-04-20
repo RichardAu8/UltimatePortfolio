98426cc575ee5a450b50cc966d3c666ff0ad4335
Richard Au <richardwau@gmail.com>
2022-04-20 14:47:23 +1000

    17. Creating the HomeView - complete

---
+++ UltimatePortfolio/Color-Additions.swift
@@ -0,0 +1,15 @@
+//
+//  Color-Additions.swift
+//  UltimatePortfolio
+//
+//  Created by RICHARD AU on 20/4/2022.
+//
+
+import Foundation
+import UIKit
+import SwiftUI
+
+extension Color {
+    static let systemGroupedBackground = Color(UIColor.systemGroupedBackground)
+    static let secondarySystemGroupedBackground = Color(UIColor.secondarySystemGroupedBackground)
+}
